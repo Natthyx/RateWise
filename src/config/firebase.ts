e58aba34c57ad916d40a9062b5a869e4252ce1a8
Natthyx <natannanz16@gmail.com>
2025-10-24 11:35:36 +0300

    update: updated the createservice logic to create only by superadmin and admin assigned to that service

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,8 +5,8 @@ import path from "path";
 
 
 dotenv.config();
-const serviceAccount = require(path.resolve("./serviceAccountKey.json"));
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+// const serviceAccount = require(path.resolve("./serviceAccountKey.json"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET || ""
